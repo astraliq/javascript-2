ee41c66d3ab176e55a1efbac0ae0d65b428ea5ce
astraliq <astraliq457@gmail.com>
2019-06-16 09:22:08 +0300

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const fs = require('fs');
 const cart = require('./cartRouter');
 const app = express();
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 app.use(express.json());
 app.use('/', express.static('dist/public'));
 app.use('/api/cart', cart);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/api/products', (req, res) => {
     fs.readFile('dist/server/db/catalog.json', 'utf-8', (err, data) => {
@@ -31,4 +35,4 @@ app.get('/api/products', (req, res) => {
 //     // res.send(req.params.id);
 //     res.send(req.query);
 // });
-app.listen(3000, () => console.log('Listen on port 3000...'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
